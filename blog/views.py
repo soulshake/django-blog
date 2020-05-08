ce5522b76336fe67822da91bc4a8cb0fc92109fe
cannelier <anneclaire.hosi@gmail.com>
2020-05-08 16:42:09 +0200

    Modified templates to display posts from db

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):
-	posts = Post.objects.all()
-	return render(request,'blog/post_list.html', {'posts':posts})
+	posts = Post.objects.all() #crée l'objet liste de posts à partir du modèle Post
+	return render(request,'blog/post_list.html', {'posts':posts}) #passe posts en argument au template
 
 
