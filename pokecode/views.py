e5ab02b6a51545bc1af65075853ee3128a65daaa
cannelier <anneclaire.hosi@gmail.com>
2020-05-09 10:38:28 +0200

    Un peu plus joli

--- pokecode/views.py
+++ pokecode/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+	posts = Post.objects.all() #crée l'objet liste de posts à partir du modèle Post
+	return render(request,'blog/post_list.html', {'posts':posts}) #passe posts en argument au template
